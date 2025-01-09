5b04944db1842f2a7ae7428bee9495ee7480203b
d3m0n <th3l1ghtd3m0n@gmail.com>
2025-01-09 12:26:47 -0300

    feat(button): add start reading button

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,12 +1,15 @@
 const Hero = () => {
   return (
     <div className="bg-[var(--homepage-main-color)] p-6">
-      <h2 className="hero-text text-8xl font-normal tracking-tighter mb-4">
+      <h2 className="hero-text text-8xl text-black font-normal tracking-tighter mb-4">
         Stay curious.
       </h2>
-      <h3 className="hero-subtext text-2xl font-normal max-w-[500px] text-gray-800/80">
+      <h3 className="hero-subtext text-2xl font-normal max-w-md mb-12 text-black/90">
         Discover stories, thinking, and expertise from writers on any topic.
       </h3>
+      <button className="w-52 p-3 leading-6 text-xl bg-black/95 border border-black/90 rounded-full text-white">
+        Start reading
+      </button>
     </div>
   );
 };
