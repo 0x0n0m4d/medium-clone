1617284071436a5f85213660be9503af736f0170
d3m0n <th3l1ghtd3m0n@gmail.com>
2025-01-09 11:03:52 -0300

    feat(hero.tsx): improve hero component

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,9 +1,12 @@
 const Hero = () => {
   return (
-    <div>
-      <h2 className="font-hero text-8xl font-normal tracking-tighter bg-[var(--homepage-main-color)]">
+    <div className="bg-[var(--homepage-main-color)] p-6">
+      <h2 className="hero-text text-8xl font-normal tracking-tighter mb-4">
         Stay curious.
       </h2>
+      <h3 className="hero-subtext text-2xl font-normal max-w-[500px] text-gray-800/80">
+        Discover stories, thinking, and expertise from writers on any topic.
+      </h3>
     </div>
   );
 };
