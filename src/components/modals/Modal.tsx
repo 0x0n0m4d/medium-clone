c836ab3276d948a7a37f1eb8caac51d943d5e052
d3m0n <th3l1ghtd3m0n@gmail.com>
2025-01-10 07:00:14 -0300

    feat(modal): add close modal button

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -2,10 +2,18 @@ import { useContext } from 'react';
 import ModalContext from './ModalContext';
 
 const Modal = () => {
-  const { isModalOpen, modalContent } = useContext(ModalContext);
+  const { isModalOpen, modalContent, setModalOpen } = useContext(ModalContext);
   return isModalOpen ? (
     <div className="fixed left-0 right-0 top-0 bottom-0 bg-white/95 z-[1] flex justify-center align-center overflow-x-hidden overflow-y-auto">
-      {modalContent}
+      <div className="relative rounded bg-white px-11 py-14 shadow-black/15 shadow-[0px_2px_10px]">
+        <button
+          className="absolute top-3 right-3 text-gray-400/60 text-2xl"
+          onClick={() => setModalOpen(false)}
+        >
+          <i className="fa-solid fa-xmark" />
+        </button>
+        {modalContent}
+      </div>
     </div>
   ) : null;
 };
