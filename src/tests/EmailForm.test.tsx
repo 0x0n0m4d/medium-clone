ab706cc116f29b74c3587173b208f03ae2df411a
d3m0n <th3l1ghtd3m0n@gmail.com>
2025-01-13 16:49:53 -0300

    fix(test): fix name & bug with `toHaveValue`

--- src/tests/EmailForm.test.tsx
+++ src/tests/EmailForm.test.tsx
@@ -1,7 +1,7 @@
-import '@testing-library/dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import EmailForm from '../components/forms/EmailForm';
+import '@testing-library/dom';
 
 const mockId = 'test-id';
 const mockOnSubmit = jest.fn();
@@ -13,7 +13,7 @@ it('should render correct input values and call onSubmit with email', async () =
 
   await userEvent.type(input, 'n0m4d@root.pwn');
 
-  expect(input).toHaveValue('n0m4d@root.pwn');
+  expect(input).toHaveProperty('value', 'n0m4d@root.pwn');
 
   const submit = screen.getByRole('button', { name: /Continue/i });
 
