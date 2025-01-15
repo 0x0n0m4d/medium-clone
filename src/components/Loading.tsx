82de6c79255c8dbb3d73e732487e94fa1f04252d
d3m0n <th3l1ghtd3m0n@gmail.com>
2025-01-15 11:44:24 -0300

    feat(loading): add `Loading` component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from './Logo';
+
+const Loading = () => {
+  return (
+    <div className="w-full h-screen min-h-[542px] flex flex-col items-center justify-center bg-white">
+      <Link
+        href={`${process.env.NEXT_PUBLIC_URL}`}
+        className="min-h-[65px] w-full bg-white fixed top-0 py-10"
+      >
+        <span className="flex w-full h-full justify-center items-center">
+          <Logo />
+        </span>
+      </Link>
+      <Image
+        src="/assets/cube.gif"
+        alt="Loading Thumb"
+        width={400}
+        height={400}
+        unoptimized
+        className="w-[400px] h-[400px]"
+      />
+    </div>
+  );
+};
+
+export default Loading;
