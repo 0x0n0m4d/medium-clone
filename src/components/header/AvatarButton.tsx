fd1a7ce3b63c7ec4450f698982ea2a310952cc93
d3m0n <th3l1ghtd3m0n@gmail.com>
2025-02-12 17:04:10 -0300

    fix(button): fix button error

--- src/components/header/AvatarButton.tsx
+++ src/components/header/AvatarButton.tsx
@@ -8,17 +8,15 @@ import OpenModalButton from '../modals/OpenModalButton';
 const AvatarButton = () => {
   return (
     <DropdownMenu.Root>
-      <DropdownMenu.Trigger className="outline-none">
-        <button className="relative border border-solid border-gray-200 rounded-full">
-          <div className="w-full h-full rounded-full absolute hover:bg-black/10" />
-          <Image
-            src="/assets/default-avatar.png"
-            alt="Avatar"
-            width={32}
-            height={32}
-            className="rounded-full"
-          />
-        </button>
+      <DropdownMenu.Trigger className="outline-none relative border border-solid border-gray-200 rounded-full">
+        <div className="w-full h-full rounded-full absolute hover:bg-black/10" />
+        <Image
+          src="/assets/default-avatar.png"
+          alt="Avatar"
+          width={32}
+          height={32}
+          className="rounded-full"
+        />
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
         <DropdownMenu.Content
